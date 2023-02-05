fd838b79efd6c992a07084cc5ea8a3b71f15be03
illizard0254@gmail.com <57248121+illizard@users.noreply.github.com>
2023-02-06 00:06:16 +0900

    food classification

--- code/datasets.py
+++ code/datasets.py
@@ -8,46 +8,21 @@ from torch.utils.data import Dataset
 
 class FoodKT(Dataset):
     def __init__(self, args, img_paths, labels=None, mode='train'):
-        self.aug_ver = args.aug_ver if args.aug_ver is not None else 0
+        self.aug_ver = args.aug_ver
         self.mode = mode
         self.img_size = args.img_size
         self.img_paths = img_paths
         self.labels = labels
 
-#########################0927_wonil##################################################################
         if self.mode == 'train':
-            if self.aug_ver == 5:
+            if self.aug_ver == 0:
                 self.transform = A.Compose([
-                    A.RandomResizedCrop(height=self.img_size, width=self.img_size,
-                                        scale=(0.5, 1.0), ratio=(0.75, 1.3333333333333333),
-                                        interpolation=1, p=1.0),
-                    A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.1, rotate_limit=30,
-                                       interpolation=1, border_mode=0, value=0, p=0.5),
-                    A.HorizontalFlip(p=0.5),
-                    A.VerticalFlip(p=0.5),
-                    A.RandomBrightnessContrast(brightness_limit=(-0.1, 0.1), contrast_limit=(-0.1, 0.1), p=0.25),
-                    A.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD),
-                    ToTensorV2()
-                ])
-############################################################################################################################
-       # if self.mode == 'train':
-            elif self.aug_ver == 0:
-                self.transform = A.Compose([
-                    A.RandomResizedCrop(height=self.img_size, width=self.img_size,
-                                        scale=(0.5, 1.0), ratio=(0.75, 1.3333333333333333),
-                                        interpolation=1, p=1.0),
-                    A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.1, rotate_limit=30,
-                                       interpolation=1, border_mode=0, value=0, p=0.5),
-                    A.HorizontalFlip(p=0.5),
-                    A.VerticalFlip(p=0.5),
-                    A.OneOf([
-                        A.CLAHE(clip_limit=2, p=1.0),
-                        A.Sharpen(p=1.0),
-                        A.RandomBrightnessContrast(brightness_limit=(-0.1, 0.1), contrast_limit=(-0.1, 0.1), p=1.0),
-                    ], p=0.25),
-                    A.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD),
-                    ToTensorV2()
-                ])
+                A.Resize(self.img_size, self.img_size),
+                A.HorizontalFlip(p=0.5),
+                A.VerticalFlip(p=0.5),
+                A.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD),
+                ToTensorV2()
+            ])
             elif self.aug_ver == 1:
                 self.transform = A.Compose([
                     A.RandomResizedCrop(height=self.img_size, width=self.img_size,
@@ -109,20 +84,20 @@ class FoodKT(Dataset):
                 ])
             elif self.aug_ver == 5:
                 self.transform = A.Compose([
-                    # A.RandomResizedCrop(height=self.img_size, width=self.img_size,
-                    #                     scale=(0.5, 1.0), ratio=(0.75, 1.3333333333333333),
-                    #                     interpolation=1, p=1.0),
-                    # A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.1, rotate_limit=30,
-                    #                    interpolation=1, border_mode=0, value=0, p=0.5),
-                    # A.HorizontalFlip(p=0.5),
-                    # A.VerticalFlip(p=0.5),
-                    # A.OneOf([
-                    #     A.CLAHE(clip_limit=2, p=1.0),
-                    #     A.Sharpen(p=1.0),
-                    #     A.RandomBrightnessContrast(brightness_limit=(-0.1, 0.1), contrast_limit=(-0.1, 0.1), p=1.0),
-                    # ], p=0.25),
-                    # A.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD),
-                    # ToTensorV2()
+                    A.RandomResizedCrop(height=self.img_size, width=self.img_size,
+                                        scale=(0.5, 1.0), ratio=(0.75, 1.3333333333333333),
+                                        interpolation=1, p=1.0),
+                    A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.1, rotate_limit=30,
+                                       interpolation=1, border_mode=0, value=0, p=0.5),
+                    A.HorizontalFlip(p=0.5),
+                    A.VerticalFlip(p=0.5),
+                    A.OneOf([
+                        A.CLAHE(clip_limit=2, p=1.0),
+                        A.Sharpen(p=1.0),
+                        A.RandomBrightnessContrast(brightness_limit=(-0.1, 0.1), contrast_limit=(-0.1, 0.1), p=1.0),
+                    ], p=0.25),
+                    A.Normalize(mean=IMAGENET_DEFAULT_MEAN, std=IMAGENET_DEFAULT_STD),
+                    ToTensorV2()
                 ])
             elif self.aug_ver == 6:
                 self.transform = A.Compose([
