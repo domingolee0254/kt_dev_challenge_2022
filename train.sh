f9c0edb177ec0f71cbca375ad3be63739c1274c4
illizard0254@gmail.com <57248121+illizard@users.noreply.github.com>
2023-02-05 23:50:12 +0900

    food classification

--- train.sh
+++ train.sh
@@ -2,4 +2,4 @@ python code/train.py -dd '/home/work/team01/food-kt/data' -sd '/home/work/team01
     -m 'tf_efficientnet_b4_ns' -is 384 -av 0 -e 100 -we 10 -bs 32 -nw 4 \
     -l 'smoothing_ce' -ls  0.5 -ot 'adamw' -lr 1e-3 -sc 'cos_base' -wd 0.05 \
     -cm True -mp 0.5 -cms 51 \
-    --wandb False --amp True;
+    --wandb False --amp True
